abe7225faa0731cd9b28b6955624c2ca86b2ed1b
laon0125 <jinu0125@gmail.com>
2023-04-05 10:25:45 +0900

    push

---
+++ src/main/java/com/example/webSocketDemo/WebController.java
@@ -0,0 +1,15 @@
+package com.example.webSocketDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class WebController {
+    @RequestMapping("/mychatt")
+    public ModelAndView chatt() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("chatting");
+        return mv;
+    }
+}
